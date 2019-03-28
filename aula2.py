4ecc41f17a991e47b0d99443b3400b46f4175efa
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2019-03-28 16:52:04 -0300

    Create aula2.py

---
+++ aula2.py
@@ -0,0 +1,20 @@
+Aula 2
+
+#1) Fazer um programa que leia dois números e mosta a soma
+a = float(input('digite um número:'))
+b = float(input('digite outro número:')
+c = float(a + b)
+print('c:',c)
+
+#2) Fazer um programa que leia quatro números e imprima a média
+
+
+#3) Fazer uma programa que mostre o tempo de 5000 segundos em hora, minuto e segundo
+
+totalseg = input ('tempo em segundos:')
+hora = totalseg//3600
+a = (totalseg % 3600)
+minuto = a//60
+segundo = a % 60
+
+print (hora,':',minuto,':',segundo
