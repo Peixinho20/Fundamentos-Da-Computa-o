3786492dae0a4291f06a2e5fd7c30b78089d4cfe
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2019-04-24 23:00:13 -0300

    Update ex003l1.py

--- ex003l1.py
+++ ex003l1.py
@@ -5,27 +5,20 @@ média aritmética dos números pares. A leitura deve ser encerrada quando for l
 número zero, que não deve ser considerado.
 '''
 
-n=0; cont_p=0; cont_i=0; media_p=0;
-flag=0; #é usado pra que?
+a = 0
+par = 0
+impar = 0
+media_par = 0
+total = 0
 
-n = int(input("Entre com um inteiro: "))
-
-if n!=0:
-	flag=1
-
-while flag:
-	if n%2==0:
-		cont_p +=1 #mais igual (+=)
-		media_p +=n
-	else:
-		cont_i +=1
-	
-	n = int(input("Entre com um inteiro: "))
-	
-	if n==0:
-		flag=0
-	
-if cont_p!=0:
-	media_p = media_p/cont_p
-
-print 'quantidade de números pares: %d \nQuantidade de números ímpares: %d \n Média de números pares: %d' %(cont_p, cont_i, media_p)
+a = int(input('entre com um número inteiro:'))
+while a > 0:
+   if a%2 == 0:
+       par = par + 1
+       total = total + 1
+   else:
+       impar = impar + 1
+       total = total + 1
+   a = int(input('entre com um número inteiro:'))
+print('O total de par é', par, 'media de par é', par/total)
+print('O total de impar é', impar,'media de impar é', impar/total)
