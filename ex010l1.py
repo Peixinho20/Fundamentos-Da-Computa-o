d190ed395201b6772fb1c5a9cf74e7f0798392d3
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2019-06-10 09:34:19 -0300

    Create ex010l1.py

---
+++ ex010l1.py
@@ -0,0 +1,19 @@
+sim = 0
+nao = 0
+psim = 0
+pnao = 0
+soma_sim = 0
+soma_nao = 0
+
+while sim == 's':
+    a = str(input('Está satisfeito com os serviços? (s ou n): '))
+    soma_sim = sim + 1
+    psim = soma_sim/100
+    a = str(input('Está satisfeito com os serviços? (s ou n): '))
+while nao == 'n':
+    a = str(input('Está satisfeito com os serviços? (s ou n): '))
+    soma_nao = nao + 1
+    pnao = soma_nao/100
+    a = str(input('Está satisfeito com os serviços? (s ou n): '))
+
+print('A porcentagem que disse sim é', psim, '%, e a porcentagem que disse não é', pnao, '%.')    
