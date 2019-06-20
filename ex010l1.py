a1a13392b436edbea6774f32560d69ed83d3d1c0
Peixinho20 <48596482+Peixinho20@users.noreply.github.com>
2019-06-20 07:53:15 -0300

    Update ex010l1.py

--- ex010l1.py
+++ ex010l1.py
@@ -1,3 +1,9 @@
+'''
+Uma empresa lançou um novo produto no mercado e fez uma pesquisa para saber se os  consumidores  estavam  satisfeitos,  para  
+isso  eles  deveriam  responder  sim ‘S’ou não ‘N’. Crieum algoritmoque  leia  a  resposta  de  todas as pessoas  e  escreva  a 
+porcentagem dos que disseram sim e dos que disseram não.Obs: O final da leitura de dados é marcado pela resposta ‘F’
+'''
+
 sim = 0
 nao = 0
 psim = 0
